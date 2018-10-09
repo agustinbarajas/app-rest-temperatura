28195011b8b9e952ef740425d60e9d22d7a036ac
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-09 22:44:16 +0000

    Cambios de los nombres de acceso en el controlador

--- controllers/temperaturaController.js
+++ controllers/temperaturaController.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Temperatura = mongose.model('Temperatura');
 
 //GET - Retorna la ultima temperatura registrada
-exports.findLastTemperature = function(req, res) {
+exports.getLastTemperature = function(req, res) {
 	Temperatura.find().sort([['fecha': -1]]).exec((err, tempe){
-		if(err) return res.send(500, err.message);
-		res.status(200).jsonp(tempe);
+		if(err) return res.status(500).send(err);
+		res.status(200).json(tempe);
 	});
 }
 //POST - Guarda un registro
@@ -15,7 +15,7 @@ exports.addTemperature = function(req, res) {
 		fecha: new Date()
 	});
 	temperatura.save((err, tempe) {
-		if(err) return res.send(500, err.message);
-		res.status(200).jsonp(tempe);
+		if(err) return res.status(500).send(err);
+		res.status(200).json(tempe);
 	});
 }
