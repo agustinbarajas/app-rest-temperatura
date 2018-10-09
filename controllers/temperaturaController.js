8b0f1a400247c7c9342ff912c2af8bf64e85c96b
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-09 23:28:08 +0000

    Cambios en el controlador

--- controllers/temperaturaController.js
+++ controllers/temperaturaController.js
@@ -1,20 +1,19 @@
-var mongoose = require('mongoose');
-var Temperatura = mongose.model('Temperatura');
+var Temperatura = require("../models/temperatura");
 
 //GET - Retorna la ultima temperatura registrada
-exports.getLastTemperature = function(req, res) {
-	Temperatura.find().sort([['fecha': -1]]).exec((err, tempe){
+exports.getLastTemperature = (req, res) => {
+	Temperatura.find().sort([['fecha', -1]]).exec((err, tempe) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
 }
 //POST - Guarda un registro
-exports.addTemperature = function(req, res) {
+exports.addTemperature = (req, res) => {
 	var temperatura = new Temperatura({
 		temperatura: req.body.temperatura,
 		fecha: new Date()
 	});
-	temperatura.save((err, tempe) {
+	temperatura.save((err, tempe) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
