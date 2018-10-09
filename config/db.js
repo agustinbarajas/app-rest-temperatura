829bd673a9e7d6509b3f3939e3d2442bd8c683e8
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-09 23:26:37 +0000

    Cambios en conexión a bd

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-const dbURI = "mongodb://admin:4hMOi0JK3rwzmuaz@cluster0-shard-00-00-9lajs.mongodb.net:27017,cluster0-shard-00-01-9lajs.mongodb.net:27017,cluster0-shard-00-02-9lajs.mongodb.net:27017/temperatura?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+const dbURI = "mongodb://admin:p6bYxktV6w156nWM@cluster0-shard-00-00-9lajs.mongodb.net:27017,cluster0-shard-00-01-9lajs.mongodb.net:27017,cluster0-shard-00-02-9lajs.mongodb.net:27017/temperatura?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 
 const options = {
-	reconnetTries: Number.MAX_VALUE,
 	poolSize: 10
 };
 
