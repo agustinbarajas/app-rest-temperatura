e76c506c0c06116bc290e9fa27c6c6ccde94afa3
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-10 00:56:37 +0000

    Cambio en la forma de hacer el find en el controlador

--- controllers/temperaturaController.js
+++ controllers/temperaturaController.js
@@ -2,7 +2,7 @@ var Temperatura = require("../models/temperatura");
 
 //GET - Retorna la ultima temperatura registrada
 exports.getLastTemperature = (req, res) => {
-	Temperatura.find().sort([['fecha', -1]]).exec((err, tempe) => {
+	Temperatura.find({},['temperatura', 'fecha'],{limit:1,sort:{fecha: -1}},(err, tempe) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
