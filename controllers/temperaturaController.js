08e17a3c07ff1924704c2445a15756d990d6a90c
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-10 17:09:02 +0000

    Agregado método para obtener el histórico de todas las temperaturas

--- controllers/temperaturaController.js
+++ controllers/temperaturaController.js
@@ -1,12 +1,19 @@
 var Temperatura = require("../models/temperatura");
 
-//GET - Retorna la ultima temperatura registrada
+//GET - Retorna la última temperatura registrada
 exports.getLastTemperature = (req, res) => {
 	Temperatura.find({},['temperatura', 'fecha'],{limit:1,sort:{fecha: -1}},(err, tempe) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
-}
+};
+//Get - Retorna histórico de temperaturas
+exports.getAllTemperatures = (req, res) => {
+    Temperatura.find({},['temperatura', 'fecha'],{sort:{fecha: 1}}, (err, tempe) => {
+        if(err) return res.status(500).send(err);
+        res.status(200).json(tempe);
+    });
+};
 //POST - Guarda un registro
 exports.addTemperature = (req, res) => {
 	var temperatura = new Temperatura({
@@ -17,4 +24,4 @@ exports.addTemperature = (req, res) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
-}
+};
