41654202b4184e4e57135333789d8fa6012bf334
Agustin Barajas Valdivia <abv1703@hotmail.com>
2018-10-11 07:34:05 +0000

    Agregado método que retorna las temperaturas de los últimos dos dias

--- servidor-rest/controllers/temperaturaController.js
+++ servidor-rest/controllers/temperaturaController.js
@@ -2,14 +2,23 @@ var Temperatura = require("../models/temperatura");
 
 //GET - Retorna la última temperatura registrada
 exports.getLastTemperature = (req, res) => {
-	Temperatura.find({},['temperatura', 'fecha'],{limit:1,sort:{fecha: -1}},(err, tempe) => {
+	Temperatura.find({},{'temperatura':1, 'fecha':1, '_id':0},{limit:1,sort:{fecha: -1}},(err, tempe) => {
 		if(err) return res.status(500).send(err);
 		res.status(200).json(tempe);
 	});
 };
 //Get - Retorna histórico de temperaturas
 exports.getAllTemperatures = (req, res) => {
-    Temperatura.find({},['temperatura', 'fecha'],{sort:{fecha: 1}}, (err, tempe) => {
+    Temperatura.find({},{'temperatura':1, 'fecha':1, '_id':0},{sort:{fecha: 1}}, (err, tempe) => {
+        if(err) return res.status(500).send(err);
+        res.status(200).json(tempe);
+    });
+};
+//Get - Retorna las temperaturas de los últimos dos dias
+const fechaHoy = new Date();
+var fecha = new Date(fechaHoy.getFullYear(), fechaHoy.getMonth(), fechaHoy.getDate() - 2);
+exports.getTemperatureLastTwoDays = (req, res) => {
+    Temperatura.find({'fecha': {'$gte': fecha}},{'temperatura':1, 'fecha':1, '_id':0},{sort:{fecha: 1}}, (err, tempe) => {
         if(err) return res.status(500).send(err);
         res.status(200).json(tempe);
     });
